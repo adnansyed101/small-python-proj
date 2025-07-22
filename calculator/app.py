a9b828debaccf62441247eb00f644ad025fcb5fa
adnansyed101 <sirgeant10@gmail.com>
2025-07-22 12:52:07 +0600

    Bare Bone structure created.

---
+++ calculator/app.py
@@ -0,0 +1,44 @@
+def add(a=0, b=0):
+    return a + b
+
+def sub(a=0, b=0):
+    return a-b
+
+def multi(a=0, b=0):
+    return a * b
+
+def div(a=0, b=0):
+    return a/b
+
+
+def calculator():
+    print('1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n5. Exit')
+
+    operation = int(input("What operation do you want to do ? \n") or False)
+
+    if not operation:
+        return print("You did not enter an operation. Please enter an operation.")
+    elif(operation == 5):
+        return exit();
+
+    numberInput1 = int(input("Enter Number 1: "))
+    numberInput2 = int(input("Enter Number 2: "))
+
+
+    match operation:
+        case 1:
+            print(add(numberInput1, numberInput2))
+        case 2:
+            print(add(numberInput1, numberInput2))
+        case 3:
+            print(add(numberInput1, numberInput2))
+        case 4:
+            print(add(numberInput1, numberInput2))
+        case 5:
+            print(add(numberInput1, numberInput2))
+            exit()
+        case _:
+            print("Something went wrong in the calculator.")
+
+
+calculator();
